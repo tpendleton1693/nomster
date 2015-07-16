0eb28cc7446780fc7f88c028750baf2c35ed7427
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-07-16 00:14:58 +0000

    Comment form added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,4 @@
 class Comment < ActiveRecord::Base
-	def change
-		create_table :comments do |t|
-			t.text :message
-			t.string :rating
-
-			t.integer :user_id
-			t.integer :place_id
-			t.timestamps
-		end
-
-		add_index :comments, [:user_id, :place_id]
-		add_index :comments, :place_id
-	end
+	belongs_to :user
+	belongs_to :place
 end
